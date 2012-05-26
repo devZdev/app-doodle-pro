9fa7cfcfbae268b51deaf0e63c00ed378e8b1eff
Devin Zimmer <dz@renttherunway.com>
2012-05-26 13:44:45 -0400

    publishing libraries and images for fancybox

--- index.php
+++ index.php
@@ -3,16 +3,29 @@
 <html>
 <head>
 <title>App Doodle Pro</title>
-<link rel="stylesheet" type="text/css" href="/css/style.css" />
-<script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js" type="text/javascript"></script>
+<link rel="stylesheet" type="text/css" href="/public/css/style.css" />
+<script type="text/javascript">
 
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-32149572-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
 </head>
 
 <body>
 <div id="wrapper">
 <h1>The Original App Doodle Pro Notebook</h1>
 <img id="main-image" src="/img/notebook.gif" alt="App Doodle Pro Notebook" />
-</div>	
+</div>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js" type="text/javascript"></script>	
+<script src="/public/js/lib/jquery.fancybox-1.3.4.pack.js" type="text/javascript"></script>
 </body>
 
 </html>
