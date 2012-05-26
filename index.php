edfd1ca4663aed97edac382039d62d0878b3d131
Devin Zimmer <dz@renttherunway.com>
2012-05-26 12:40:44 -0400

    added start up files

---
+++ index.php
@@ -0,0 +1,18 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>App Doodle Pro</title>
+<link rel="stylesheet" type="text/css" href="/css/style.css" />
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js" type="text/javascript"></script>
+
+</head>
+
+<body>
+<div id="wrapper">
+<h1>The Original App Doodle Pro Notebook</h1>
+<img id="main-image" src="/img/notebook.gif" alt="App Doodle Pro Notebook" />
+</div>	
+</body>
+
+</html>
