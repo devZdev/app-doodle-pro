71ccc383e48ba4b0f8c038c76c6fdce8f288e974
Devin Zimmer <dz@renttherunway.com>
2012-05-30 23:18:30 -0400

    Added persuasion and anecdotals

--- index.php
+++ index.php
@@ -42,20 +42,53 @@ em { font-weight: bold; padding-right: 1em; vertical-align: top; }
 				</ul>
 			</div>
 		</div>
-		<div id="content">
-			<div id="content-main">
-			<img class="main-img" src="/public/img/main-product.png" alt="The App Doodle Pro" />
-			<ul class="description">
-					<li>True to scale.</li>
-					<li>Keep your app ideas organized.</li> 
-					<li>Programmers will be more effective when your design is organized.</li> 
-					<li>Designed with the app creator in mind.</li> 
-					<li>Simple, Elegant, Classy.</li> 
-					<li>The perfect companion to the modern app designer.</li> 
-					<li>Free Shipping!</li> 
-				</ul>
-			<img class="product-open shadow" src="/public/img/product-open-v2.png" alt="" />
-			</div>
+		<div class="content">
+			<div class="content-main">
+                <img class="main-img" src="/public/img/main-product.png" alt="The App Doodle Pro" />
+                <ul class="description">
+                        <li>True to scale.</li>
+                        <li>Keep your app ideas organized.</li> 
+                        <li>Programmers will be more effective when your design is organized.</li> 
+                        <li>Designed with the app creator in mind.</li> 
+                        <li>Simple, Elegant, Classy.</li> 
+                        <li>The perfect companion to the modern app designer.</li> 
+                        <li>Free Shipping!</li> 
+                    </ul>
+                <img class="product-open shadow" src="/public/img/product-open-v2.png" alt="" />    
+                <div class="stories">
+                    <h4>Look and Feel, Insperation, Moleskine, Elegance</h4>
+                
+                    <p>"The love that the App Doodle people partnered up with Moleskine. Something about the simple, lightweight, and classy feel to this notebook inspires me to create!"</p>
+                
+                
+                    <h4>Convenience</h4>
+                
+                    <p>"I used to scribble app ideas onto bar napkins. There was something gritty and fun about that, but now I carry this little book wherever I go. My ideas end up being more clear and  organized when I wake up the next day."</p>  
+                
+                
+                    <h4>Organization</h4>
+                
+                    <p>"The App Doodle Pro is a must have for any modern day app developer. Having my app organized into different views helps me keep track of every thought I put into it."</p>
+                
+                    <h4>Productivity</h4>
+                
+                    <p>"You don't have to be a programmer to make your ideas come to life. Expressing your next million dollar app to an iOS developer couldn't be easier or more effective. Having each angle of my app designed ahead of time and organized in this book helped my programmer finish the app weeks before the deadline."</p>
+                
+                    <h4>Save Yourself Time and Money</h4>
+                
+                    <p>"Everybody in the app world knows that when you make a programmer change course half way through the development cycle, nobody wins. This notebook really helped me really iron out the kinks of my app in the design process which saved me time and money."</p>
+            
+                </div>
+                <div class="persuasion">
+                    <h3>"Design your app with this notebook, send development elsewhere."</h3>
+                    <h3>"From any app idea to professional app development."</h3>
+                    <h3>"App Doodle Pro makes professionals out of amateurs."</h3>
+                    <h3>"You no longer need to be a software developer to create the next big app."</h3>
+                    <h3>"Put an App Doodle Pro in anybody's hand and you'll be surprised at what they come up with."</h3>
+                    <h3>"It's the catalyst for the average employee to make a change in their lives and begin creating."</h3>
+                    <h3>"Puts the next big app in your hands."</h3>
+                </div>
+            </div>           
 			<div id="content-sidebar">
 				<ul class="pricing">
 					<li>App Doodle Pro (Pack of 3)</li>
@@ -80,25 +113,25 @@ em { font-weight: bold; padding-right: 1em; vertical-align: top; }
 			<h3>Shopping Cart</h3>
             <h5>Shipping Information</h5>
 			<div class="cart">
-			<img src="/public/img/product-cart.png" alt="" />
-			<table id="totals">
-				<tr>
-					<td>Quantity:</td>
-					<td><input type="text" name="quantity" value="1" /></td>
-				</tr>
-				<tr>
-					<td>Sub-Total:</td>
-					<td>$22.99</td>
-				</tr>
-				<tr>
-					<td>Shipping:</td>
-					<td>$3.99</td>
-				</tr>
-				<tr>
-					<td>Grand Total:</td>
-					<td>$26.98</td>
-				</tr>
-			</table>
+                <img src="/public/img/product-cart.png" alt="" />
+                
+               
+                <p>App Doodle Pro (Bundle of three)</p>
+                <p>Quantity: </p><input type="text" name="quantity" value="1" /> 
+                <table id="totals">
+                    <tr>
+                        <td>Sub-Total:</td>
+                        <td>$22.99</td>
+                    </tr>
+                    <tr>
+                        <td>Shipping:</td>
+                        <td>$3.99</td>
+                    </tr>
+                    <tr>
+                        <td>Grand Total:</td>
+                        <td>$26.98</td>
+                    </tr>
+                </table>
 			</div>
 			<form id="shipping-form" action="shipping.php" method="post">
 				
