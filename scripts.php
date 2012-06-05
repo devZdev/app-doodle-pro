da05db6afefc36a4b5019de70742d0df392db00d
Devin Zimmer <dz@renttherunway.com>
2012-06-05 10:28:49 -0400

    adding jquery form plugin

--- scripts.php
+++ scripts.php
@@ -23,5 +23,7 @@ if($_SERVER['SERVER_NAME']!="localhost"){?>
 <script src="/public/js/lib/jquery.fancybox-1.3.4.pack.js" type="text/javascript"></script>
 <script src="/public/js/lib/jquery.accordion.min.js" type="text/javascript"></script>
 <script src="/public/js/lib/modernizer.js" type="text/javascript"></script>
+<script src="/public/js/lib/jquery.form.js" type="text/javascript"></script>
 <script src="/public/js/document.events.js" type="text/javascript"></script>
 <script src="/public/js/tracking.js" type="text/javascript"></script>
+
