824d73939695b23461a1df3df2b278c1599ac57e
Devin Zimmer <dz@renttherunway.com>
2012-06-07 19:24:34 -0400

    revamp of quotes and style

--- public/js/globals.js
+++ public/js/globals.js
@@ -1,10 +1,12 @@
 //@dz
 //globals.js - define global scope variables under the globals object
 var globals = {};
+globals.credit = {};
 globals.hostname = window.location.hostname;
 globals.developer = "Devin Zimmer";
 globals.grandTotal = 26.98;
 globals.subTotal = 22.99;
 globals.price = 22.99;
+
 
 
--- views/home.php
+++ views/home.php
@@ -20,38 +20,30 @@
 		<li>The perfect companion to the modern app designer.</li> 
 		<li>Free Shipping!</li> 
 	</ul>
+    <div class="persuasion">
+        <h3>Your next million dollar App idea starts here..</h3>
+    </div>
+    <div class="open-view"> 
     <img class="product-open shadow" src="/public/img/product-open-v2.png" alt="" />    
-               	<div class="stories">
-                    <h4>Look and Feel, Insperation, Moleskine, Elegance</h4>
-                
-                    <p>"The love that the App Doodle people partnered up with Moleskine. Something about the simple, lightweight, and classy feel to this notebook inspires me to create!"</p>
-                
-                
+    </div>
+                <div class="keynote keynote-left">
                     <h4>Convenience</h4>
-                
-                    <p>"I used to scribble app ideas onto bar napkins. There was something gritty and fun about that, but now I carry this little book wherever I go. My ideas end up being more clear and  organized when I wake up the next day."</p>  
-                
-                
-                    <h4>Organization</h4>
-                
-                    <p>"The App Doodle Pro is a must have for any modern day app developer. Having my app organized into different views helps me keep track of every thought I put into it."</p>
-                
-                    <h4>Productivity</h4>
-                
-                    <p>"You don't have to be a programmer to make your ideas come to life. Expressing your next million dollar app to an iOS developer couldn't be easier or more effective. Having each angle of my app designed ahead of time and organized in this book helped my programmer finish the app weeks before the deadline."</p>
-                
-                    <h4>Save Yourself Time and Money</h4>
-                
-                    <p>"Everybody in the app world knows that when you make a programmer change course half way through the development cycle, nobody wins. This notebook really helped me really iron out the kinks of my app in the design process which saved me time and money."</p>
-            
+                    <p class="editorial">"I used to scribble app ideas onto bar napkins. There was something gritty and fun about that, but now I carry this little book wherever I go. My ideas end up being more clear and  organized when I wake up the next day."</p>
+                    <p>Nizar Dhamani - iOS Developer and Creator of Casual Math, Casual Pong, and Two Sticks.</p>
                 </div>
-                <div class="persuasion">
-                    <h3>"Design your app with this notebook, send development elsewhere."</h3>
-                    <h3>"From any app idea to professional app development."</h3>
-                    <h3>"App Doodle Pro makes professionals out of amateurs."</h3>
-                    <h3>"You no longer need to be a software developer to create the next big app."</h3>
-                    <h3>"Put an App Doodle Pro in anybody's hand and you'll be surprised at what they come up with."</h3>
-                    <h3>"It's the catalyst for the average employee to make a change in their lives and begin creating."</h3>
-                    <h3>"Puts the next big app in your hands."</h3>
+                <div class="keynote keynote-right">    
+                    <h4>Insperation, Elegance, Moleskine</h4>
+                    <p class="editorial">"I love that the App Doodle people partnered up with Moleskine. The notebook is simple, lightweight, and classy. It inspires me to create!"</p>
+                    <p>Jenny Bergen - Features Writer, PC Magazine</p>
+                </div>
+                <div class="keynote keynote-left">     
+                    <h4>Productivity</h4>
+                    <p class="editorial">"You don't have to be a programmer to make your ideas come to life. Expressing your next million dollar app to an iOS developer couldn't be easier or more effective. Having each angle of my app designed ahead of time and organized in this book helped my programmer finish the app weeks before the deadline."</p>
+                    <p>Christopher Sun - App Developer and Software Engineer, Rent the Runway</p>
+                </div>
+                <div class="keynote keynote-right">    
+                    <h4>Save Yourself Time and Money</h4>                
+                    <p class="editorial">"Everybody creating apps knows that when you direct your programmer to change course half way through the development cycle, progress gets delayed and the working relationship is never the same. This notebook really helps the designer iron out the kinks of an app in the design process, which prevents a lot of headaches."</p>
+                    <p>Isaac Elbaz - Software Engineer, The FreeBSD Project</p> 
                 </div>
             </div>
