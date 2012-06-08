62c7311b396f699c7cfa9a004782c56363c8b2bf
Devin Zimmer <dz@renttherunway.com>
2012-06-08 14:19:43 -0400

    sidebar text change

--- sidebar.php
+++ sidebar.php
@@ -9,7 +9,7 @@
 
 <div id="content-sidebar">
 	<ul class="pricing">
-		<li>App Doodle Pro (Pack of 3)</li>
+		<li>App Doodle Pro (bundle of 3)</li>
 		<li>$22.99</li>
 	</ul>
 	<a class="buy-button" href="#checkout">BUY NOW</a>
