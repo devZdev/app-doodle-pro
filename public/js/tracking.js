784ddf657b5d3c5adc41b129c06f7fda01a14621
Devin Zimmer <dz@renttherunway.com>
2012-06-10 20:20:09 -0400

    event tracking on buy/continue

--- public/js/tracking.js
+++ public/js/tracking.js
@@ -1,14 +1,23 @@
 //Event Tracking
 $(document).ready(function(){
 	
-	//Buy Button
-	$('#buy-button').click(function(){
-		trackEvent("Main", "click-buy");
-	});
+	//Buy Button sidebar
+    $('.buy-button').parent('#content-sidebar').children('.buy-button').click(function(){
+        trackEvent('Home', 'click_buy_button', 'sidebar', null, true);
+    });
+    
+     //Buy Button Header
+     $('.buy-button').parent('li').children('.buy-button').click(function(){
+        trackEvent('Home', 'click_buy_button', 'header', null, true);
+     });
 
+     //Continue with Order
+     $('#shipping-form .submit').click(function(){
+        trackEvent('Cart', 'click_continue_order', 'fancybox-submit', null, true);    
+     });
 });
 
-function trackEvent(category,action){
-	console.log('_gaq.push(['+category+','+action+']);');
-	//_gaq.push(["_trackEvent",category,action,label,value,opt_noninteraction]);
+function trackEvent(category, action, label, value, opt){
+	//console.log('_gaq.push(['+category+','+action+','+label+','+value+','+opt+']);');
+	_gaq.push(["_trackEvent",category,action,label,value,opt]);
 }
